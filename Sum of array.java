8c075cc28fbaf7d52b3a8d5cd6af6af16d1bc392
nivetithaJ2 <nivetithajayakumar@gmail.com>
2025-08-12 19:29:59 +0530

    Update Sum of array.java

--- Sum of array.java
+++ Sum of array.java
@@ -1,1 +1,13 @@
+public class SumOfArray {
+    public static void main(String[] args) {
+        int[] numbers = {5, 10, 15, 20, 25}; // initialized array
+        int sum = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            sum += numbers[i]; // add each element to sum
+        }
+
+        System.out.println("Sum of array elements: " + sum);
+    }
+}
 
